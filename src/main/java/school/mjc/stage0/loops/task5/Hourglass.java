2cb63e5821406391dc0005817e3132a740971317
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-04 21:53:31 +0400

    Update Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+       int middle = (height+1)/2;
+        int index = height;
+        for (int i=1; i<=height; i++){
+            for (int j=1; j<=(height-index+1)/2; j++)
+                System.out.print(" ");
+            for (int j=1; j<=index; j++){
+                 System.out.print(8);
+            }
+            for (int j=1; j<=(height-index+1)/2; j++)
+                System.out.print(" ");
+
+            if(middle>i) index-=2;
+            else if(middle<=i) index+=2;
+            System.out.println("");
+        }
 
     }
 }
